d1970dad7d80d5b13392a9b313f35a86d7cfb0b2
TaLePhu <lephu18062@gmail.com>
2023-11-26 16:08:40 +0700

    add redux-toolkit lan 1

---
+++ foodRecide/slices/restaurantSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  restaurant: null,
+}
+
+export const restaurantSclice = createSlice({
+  name: 'restaurant',
+  initialState,
+  reducers: {
+    
+    setRestaurant: (state, action) => {
+      state.restaurant += action.payload;
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setRestaurant } = restaurantSclice.actions;
+
+export const selectRestaurant = state=>state.restaurant.restaurant;
+
+export default restaurantSclice.reducer
